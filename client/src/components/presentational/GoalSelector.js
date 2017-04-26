e00d43b892d92b8568cc1077d6b6f79096f97ea8
zach-is-my-name <zach.greco@gmail.com>
2017-04-25 20:53:57 -0400

    GoalSelect drop-down rendering with goals from server (no event handling)

--- client/src/components/presentational/GoalSelector.js
+++ client/src/components/presentational/GoalSelector.js
@@ -1,40 +1,55 @@
-
 import React from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 
-function GoalSelector({data: {loading, error, goalDocs}}) {
-
-if (loading) {
-console.log('loading:',true);
-console.log('HIT')
-}
-
-if (error) {
-  console.log(error.message);
-}
-
-if (goalDocs)
-console.log('yes');
-console.log(goalDocs);
-return <p>testing</p>
-
-}
-
-
-const GoalQuery = gql`
-query  {
+const GoalQuery = gql `
+query root {
   goalDocs {
     goal
   }
 }
 `;
 
+function GoalSelector({
+    data: {
+        loading,
+        error,
+        goalDocs
+    }
+}) {
+    // if (loading) {
+    //   return <div>Loading</div>;
+    // } else {
+    //   return (
+    //     <ul>
+    //       {goalDocs.map(goal =>
+    //         <li>
+    //           {goal}
+    //         </li>
+    //       )}
+    //     </ul>
+    //   );
+    // }
+    const selectGoal = event => console.log(this.refs.select.value);
+
+if (loading){
+        return <div>loading...</div>;
+      } else {
+        const goalSelectInputValues = goalDocs.map((goal, index) => {
+          return   <option value={index} key={index}>{goal.goal}</option>
+        });
+
+        const goalSelectInput = <form className="goal-select" >
+          <select ref="select" onChange={selectGoal}>
+            {goalSelectInputValues}
+              </select>
+              </form>
+
+        console.log(goalDocs.map(goal => goal))
+        return (goalSelectInput)
+}
+}
+
+
 const GoalSelectorData = graphql(GoalQuery)(GoalSelector);
 export default GoalSelectorData;
-
-// if (!loading) {
-// const goalsDropDown =
-// goals.map((goal,index) => {
-//   return <option value={index} key={index}> {goal} </option>
-// })}
