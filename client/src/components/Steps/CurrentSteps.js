b694f5f1e76ef2e1859a0a40056363013b83602b
zach-is-my-name <zach.greco@gmail.com>
2017-05-17 11:03:30 -0400

    render current steps

---
+++ client/src/components/Steps/CurrentSteps.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+
+
+ class CurrentSteps extends Component {
+render() {
+   let steps =  this.props.currentGoalSteps.map((step, index) => {
+     return <li key={index}>{step}</li>
+   });
+
+  let stepsList = <ul>{steps}</ul>
+
+  console.log(this.props.text)
+  return  (<div>{this.props.currentGoalSteps ? stepsList : null}</div>)
+}}
+
+const mapStateToProps = (state, props) => {
+  return {
+    currentGoalSteps: state.goals.currentGoalSteps
+  }
+}
+
+export default connect(mapStateToProps)(CurrentSteps);
--- client/src/components/container/Steps.js
+++ client/src/components/container/Steps.js
@@ -1,12 +1,14 @@
 /* eslint-disable */
-
 import React, {Component} from 'react'
+
 import StepsInput from '../Steps/StepsInput'
+import CurrentSteps from '../Steps/CurrentSteps'
 
 export default function Steps() {
   return (
     <div>
       <StepsInput />
+      <CurrentSteps />
     </div>
   )
 }
